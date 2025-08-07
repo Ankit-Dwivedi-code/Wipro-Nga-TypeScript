d3e057b83c93f1d2b01e2d0bdf7230fcefa833fc
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2025-08-07 21:03:47 +0530

    map funtion

---
+++ map.js
@@ -0,0 +1,7 @@
+"use strict";
+let num = [2, 4, 6, 8, 10];
+num.map((value) => {
+    return value * 2;
+}).forEach((value) => {
+    console.log(value);
+});
---
+++ map.ts
@@ -0,0 +1,9 @@
+let num = [2, 4, 6, 8, 10];
+
+num.map((value) => {
+  return value * 2;
+}
+).forEach((value) => {
+  console.log(value);
+});
+
